22a1d3038eb9a2fe9fb97a9e2d094b1f6aa6a301
Pierre-Alexandre Kofron <pierrealexandre.kofron@gmail.com>
2015-04-04 13:07:27 +0200

    Fix pictures width problem. Add fake comments thread, for now to all features

--- assets/js/app.js
+++ assets/js/app.js
@@ -305,7 +305,7 @@ var cameras = L.geoJson(null, {
   },
   onEachFeature: function (feature, layer) {
     if (feature.properties) {
-      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Nom</th><td>" + feature.properties.nom + "</td></tr>" + "<tr><th>Dernière image capturée</th><td><img src='" + feature.properties.url + "'></img></td></tr>" + "<table>";
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Nom</th><td>" + feature.properties.nom + "</td></tr>" + "<tr><th>Dernière image capturée</th><td><img width='400' src='" + feature.properties.url + "'></img></td></tr>" + "<table>";
       layer.on({
         click: function (e) {
           $("#feature-title").html(feature.properties.nom);
